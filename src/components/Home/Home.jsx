3167bf46f3574063ac9a6e87bb7f85fbdead0bac
Nathanaël Fournier <nathanael@macdenathanael.home>
2024-08-19 01:43:17 +0200

    Finalisation de la page d'accueil version mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,31 +5,37 @@ import HomeParts from './HomeParts';
 const Home = () => {
   return (
     <div className="home-content">
-      <h1 className="home-title">Osez nous demander la Lune</h1>
       <video className="home-video" autoPlay loop muted playsInline>
         <source
           src="https://cdn.pixabay.com/video/2019/05/22/23882-338327769_large.mp4"
           type="video/mp4"
         />
       </video>
+      <h1 className="home-title">Osez nous demander la Lune</h1>
       <HomeParts
+        name="first-image"
         image="../src/assets/homePicture.png"
         title="Voyager au dela des frontières terrestres"
         text="Nous sommes des rêveurs qui adorons voyager. Vous revez aussi de partir
         dans l'espace ? Alors c'est le moment. Avec Space'O Agency, votre
         destination phare est à seulement quelques clics de la réalité."
+        button="En savoir plus"
       />
       <HomeParts
-        image="../src/assets/Saturne.png"
+        name="second-image"
+        image="../src/assets/planets/Saturne.png"
         title="Nos Destinations"
         text="Plusieurs destinations s'offrent à vous, des grandes étendues de Mars 
         jusqu'au plages de roche blanche de Cérès."
+        button="Voir les Planètes"
       />
       <HomeParts
+        name="third-image"
         image="../src/assets/homeShip.png"
         title="Nos Vaisseaux"
         text="Une gamme complète de vaisseaux est à votre disposition, du cargo 
         jusqu'aux vaisseaux de croisière, nous en avons pour tout les goûts."
+        button="Voir les Vaisseaux"
       />
     </div>
   );
--- src/components/Home/HomeParts.jsx
+++ src/components/Home/HomeParts.jsx
@@ -1,24 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/no-unescaped-entities */
 import PropTypes from 'prop-types';
 
-const HomeParts = ({ image, title, text }) => {
+const HomeParts = ({ name, image, title, text, button }) => {
   return (
     <div className="home-content">
-      <img
-        className="secondary-picture"
-        src={image}
-        alt="Voyageurs qui vont vers une fusée"
-      />
+      <img className={name} src={image} alt="A dynamiser" />
       <h2 className="secondary-title">{title}</h2>
       <p className="secondary-text">{text}</p>
+      <a href="#" className="home-button">
+        {button}
+      </a>
     </div>
   );
 };
 
 HomeParts.propTypes = {
+  name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  button: PropTypes.string.isRequired,
 };
 
 export default HomeParts;
