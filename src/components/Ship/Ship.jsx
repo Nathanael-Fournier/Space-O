23e789e416462498fa76d89a2747e3dcdd942ab9
Nathanael Fournier <nathanael.fournier.dev@gmail.com>
2024-09-21 14:59:18 +0200

    Récuperation et affichage des données des vaisseaux ( pas encore les pages des vaisseaux seuls ) + début de CSS pour les cartes de vaisseaux

--- src/components/Ship/Ship.jsx
+++ src/components/Ship/Ship.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import ShipCard from './ShipCard';
+
 import './Ship.scss';
 
 const Ship = () => {
-  return <div className="ship-card" />;
+  const [ships, setShips] = useState([]);
+
+  const loadShips = () => {
+    axios.get('http://localhost:8000/api/v1/ship').then((response) => {
+      setShips(response.data);
+    });
+  };
+
+  useEffect(() => {
+    loadShips();
+  }, []);
+
+  return (
+    <>
+      {ships.map((currentShip) => (
+        <ShipCard key={currentShip.id} {...currentShip} />
+      ))}
+    </>
+  );
 };
 
 export default Ship;
---
+++ src/components/Ship/ShipCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import propTypes from 'prop-types';
+
+import './Ship.scss';
+
+const ShipCard = ({ picture, name, description }) => {
+  return (
+    <div className="ship-card">
+      <img
+        className="ship-picture"
+        src={picture}
+        alt="Illustration d'un vaisseau"
+      />
+      <h2 className="ship-name">{name}</h2>
+      <p className="ship-description">{description.slice(0, 100)} ...</p>
+      <Link to="/" className="ship-button">
+        En savoir plus ...
+      </Link>
+    </div>
+  );
+};
+
+ShipCard.propTypes = {
+  picture: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
+};
+
+export default ShipCard;
