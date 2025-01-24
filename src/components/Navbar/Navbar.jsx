02316b0b4548118fab13ed6f4eb6c1e11d37c5f4
Nathanael Fournier <nathanael.fournier.dev@gmail.com>
2025-01-24 13:41:01 +0100

    Ajout de commentaires dans les composants 'Navbar' et 'NavbarLink' pour plus de clarté

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,47 @@
+// Importation de Link depuis pour la navigation entre les pages
 import { Link } from 'react-router-dom';
 
+// Importation de prop-types pour valider les types des props
 import propTypes from 'prop-types';
 
+// Importation des assets (logo et icônes)
 import logo from '../../assets/SpaceO.png';
 import planete from '../../assets/svg/planete.svg';
 import vaisseau from '../../assets/svg/vaisseau.svg';
 import devis from '../../assets/svg/devis.svg';
 import login from '../../assets/svg/login.svg';
 
+// Importation des styles spécifiques à la Navbar
 import './Navbar.scss';
 
+// Importation du composant NavbarLink pour générer des liens de navigation
 import NavbarLink from './NavbarLink';
 
+// Composant Navbar
 const Navbar = ({ isLogged, settingsIsOpen, setSettingsIsOpen }) => {
+  // Fonction pour gérer l'ouverture/fermeture du menu des paramètres
   const handleLoginClick = () => {
-    setSettingsIsOpen(!settingsIsOpen);
+    setSettingsIsOpen(!settingsIsOpen); // Inverse l'état de settingsIsOpen
   };
 
   return (
     <header>
       <nav className="navbar">
+        {/* Première partie de la navbar : lien vers la page d'accueil avec le logo */}
         <Link to="/" className="navbar-first-part">
           <img className="navbar-logo" src={logo} alt="Logo du site" />
         </Link>
+
+        {/* Deuxième partie de la navbar : liens de navigation et bouton login/logout */}
         <div className="navbar-second-part">
+          {/* Lien vers la page des planètes */}
           <NavbarLink link="/planetes" image={planete} title="Planètes" />
+          {/* Lien vers la page des vaisseaux */}
           <NavbarLink link="/vaisseaux" image={vaisseau} title="Vaisseaux" />
+          {/* Lien vers la page des devis */}
           <NavbarLink link="/devis" image={devis} title="Devis" />
+
+          {/* Bouton login/logout, affiché dynamiquement selon l'état isLogged */}
           {isLogged && (
             <button
               type="button"
@@ -61,10 +76,12 @@ const Navbar = ({ isLogged, settingsIsOpen, setSettingsIsOpen }) => {
   );
 };
 
+// Définition des types attendus pour les props du composant Navbar
 Navbar.propTypes = {
   isLogged: propTypes.bool.isRequired,
   settingsIsOpen: propTypes.bool.isRequired,
   setSettingsIsOpen: propTypes.func.isRequired,
 };
 
+// Export du composant
 export default Navbar;
--- src/components/Navbar/NavbarLink.jsx
+++ src/components/Navbar/NavbarLink.jsx
@@ -1,11 +1,15 @@
+// Importation de NavLink pour gérer les liens avec styles dynamiques
 import { NavLink } from 'react-router-dom';
 
+// Importation de PropTypes pour valider les types des props
 import PropTypes from 'prop-types';
 
+// Composant NavbarLink qui représente un lien individuel de la Navbar
 const NavbarLink = ({ link, image, title }) => {
   return (
     <NavLink
       to={link}
+      // Ajout dynamique de classes selon que le lien est actif ou non
       className={({ isActive }) =>
         isActive ? 'navbar-link navbar-link--selected' : 'navbar-link'
       }
@@ -16,10 +20,12 @@ const NavbarLink = ({ link, image, title }) => {
   );
 };
 
+// Validation des props attendues pour le composant NavbarLink
 NavbarLink.propTypes = {
   link: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
 
+// Export du composant
 export default NavbarLink;
